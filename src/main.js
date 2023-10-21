d5b0dcbec96f5b674d17a1a9a8bad8ef413080f9
Vinh Ta <tavinh132@gmail.com>
2023-10-21 14:13:20 -0700

    added bounce

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
+// your first/last name
+// mod title (e.g. Rocket Patrol Reloaded IV: The Rocketing)
+// the approximate time it took to complete the project (in hours)
+// the mods you chose from the list above, their point values, and, if necessary, an explanation of their implementation
+// citations for any sources you used (you do not need to cite Nathan or Phaser documentation)
+//  How to add physics to a constructor: https://stackoverflow.com/questions/55302007/how-add-physics-to-phaser-3-sprite
+
 // 1)    Add your own (copyright-free) looping background music to the Play scene
 //      (keep the volume low and be sure that multiple instances of your music don't play when the game restarts) (1)
 // 2)   Use Phaser's particle emitter to create a particle explosion when the rocket hits the spaceship (5)
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -32,11 +32,12 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);                                    // left
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);    // right
 
+        // set world boundaries
         this.physics.world.setBounds(borderUISize, 0, game.config.width - 2 * (borderUISize), game.config.height, true, true, false, false);
 
         // add rocket(p1)
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, "rocket").setOrigin(0.5,0);
-        this.p1Rocket.body.setCollideWorldBounds(true); //viewport edges are now walls
+        this.p1Rocket.body.setCollideWorldBounds(true, 1); //adding walls + bounce
 
 
         // define keys
